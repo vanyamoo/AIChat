29f9f903df89b21cad35cdddafb2142908369dd7
vanya <vanya.moo@gmail.com>
2025-09-11 17:19:35 +0100

    Initial Commit

---
+++ AIChat/AIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatApp.swift
+//  AIChat
+//
+//  Created by Vanya Mutafchieva on 11/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct AIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIChatTests/AIChatTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatTests.swift
+//  AIChatTests
+//
+//  Created by Vanya Mutafchieva on 11/09/2025.
+//
+
+import Testing
+@testable import AIChat
+
+struct AIChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

a15b1fe834b77d450954254b5f10f6d1ab64310d
vanya <vanya.moo@gmail.com>
2025-09-18 16:28:34 +0100

    [Feature] Add Welcome folder to Core folder, add WelcomeView file

---
+++ AIChat/Core/Welcome/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  AIChat
+//
+//  Created by Vanya Mutafchieva on 18/09/2025.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}

2e463d0cc61ae978365d84ca0aefe9d906b3ff0f
vanya <vanya.moo@gmail.com>
2025-09-22 15:36:13 +0100

    [Clean] Extract AppState to its own file

---
+++ AIChat/Core/AppView/AppState.swift
@@ -0,0 +1,40 @@
+//
+//  AppState.swift
+//  AIChat
+//
+//  Created by Vanya Mutafchieva on 22/09/2025.
+//
+import SwiftUI
+
+@Observable
+class AppState {
+    private(set) var showTabBar: Bool {
+        didSet {
+            UserDefaults.showTabbarView = showTabBar
+        }
+    }
+    
+    init(showTabBarView: Bool = UserDefaults.showTabbarView) {
+        self.showTabBar = showTabBarView
+    }
+    
+    func updateViewState(showTabBarView: Bool) {
+        showTabBar = showTabBarView
+    }
+}
+
+extension UserDefaults {
+    
+    private struct Keys {
+        static let showTabbarView = "showTabbarView"
+    }
+    
+    static var showTabbarView: Bool {
+        get {
+            standard.bool(forKey: Keys.showTabbarView)
+        }
+        set {
+            standard.set(newValue, forKey: Keys.showTabbarView)
+        }
+    }
+}

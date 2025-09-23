663dec7ba3e1a940e1bff2c315341fc6b57b1e7e
vanya <vanya.moo@gmail.com>
2025-09-23 15:27:31 +0100

    [Patch] Fix/workaround SwiftUI bug where image extends pass the view frame by applying an overlay to a `Rectangle` and clipping it

--- AIChat/Components/Images/ImageLoaderView.swift
+++ AIChat/Components/Images/ImageLoaderView.swift
@@ -14,13 +14,20 @@ struct ImageLoaderView: View {
     var resizingMode: ContentMode = .fill
     
     var body: some View {
-        WebImage(url: URL(string: urlString))
-            .resizable()
-            .indicator(.activity)
-            .aspectRatio(contentMode: resizingMode)
+        Rectangle()
+            .opacity(0)
+            .overlay(
+                WebImage(url: URL(string: urlString))
+                    .resizable()
+                    .indicator(.activity)
+                    .aspectRatio(contentMode: resizingMode)
+                    .allowsHitTesting(false)
+            )
+            .clipped()
     }
 }
 
 #Preview {
     ImageLoaderView()
+        .frame(width: 100, height: 200)
 }

986a1243b12742eb045f01a976e4aba7f3b89f34
vanya <vanya.moo@gmail.com>
2025-09-23 13:24:40 +0100

    [Feature] Add Settings folder and SettingsView file

---
+++ AIChat/Core/Settings/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  AIChat
+//
+//  Created by Vanya Mutafchieva on 23/09/2025.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}

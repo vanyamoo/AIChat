3a57783d0de1123f3056aa817ab4a14fd45e005c
vanya <vanya.moo@gmail.com>
2025-09-23 14:06:14 +0100

    [Patch] Fix sign out animation by first dismissing the sheet and adding a 1 sec delay before updating app state

--- AIChat/Core/Settings/SettingsView.swift
+++ AIChat/Core/Settings/SettingsView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct SettingsView: View {
     
+    @Environment(\.dismiss) private var dismiss
     @Environment(AppState.self) private var appState: AppState
     
     var body: some View {
@@ -26,7 +27,12 @@ struct SettingsView: View {
     
     func onSignOutTapped() {
         // do some logic to sign user out of app!
-        appState.updateViewState(showTabBarView: false)
+        dismiss()
+        
+        Task {
+            try? await Task.sleep(for: .seconds(1))
+            appState.updateViewState(showTabBarView: false)
+        }
     }
 }
 

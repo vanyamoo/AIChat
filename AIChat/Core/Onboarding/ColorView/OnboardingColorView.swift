fa4b45a8b56c5f317d16c2bcc3532d824cc019ee
vanya <vanya.moo@gmail.com>
2025-09-24 15:12:16 +0100

    [Clean] Move lazyvgrid to its own private var colorGrid

--- AIChat/Core/Onboarding/ColorView/OnboardingColorView.swift
+++ AIChat/Core/Onboarding/ColorView/OnboardingColorView.swift
@@ -14,32 +14,8 @@ struct OnboardingColorView: View {
     
     var body: some View {
         ScrollView {
-            LazyVGrid(
-                columns: Array(repeating: GridItem(.flexible(), spacing: 16), count: 3),
-                alignment: .center,
-                spacing: 16,
-                pinnedViews: [.sectionHeaders],
-                content: {
-                    Section(content: {
-                        ForEach(profileColors, id: \.self) { color in
-                            Circle()
-                                .fill(.accent)
-                                .overlay(
-                                    color
-                                        .clipShape(Circle())
-                                        .padding(selectedColor == color ? 10 : 0)
-                                )
-                                .onTapGesture {
-                                    selectedColor = color
-                                }
-                        }
-                    }, header: {
-                        Text("Select a profile color")
-                            .font(.headline)
-                    })
-                }
-            )
-            .padding(.horizontal, 24)
+            colorGrid
+                .padding(.horizontal, 24)
         }
         .safeAreaInset(edge: .bottom, alignment: .center, spacing: 16, content: {
             ZStack {
@@ -52,6 +28,34 @@ struct OnboardingColorView: View {
             .background(Color(.systemBackground))
         })
         .animation(.bouncy, value: selectedColor)
+    }
+    
+    private var colorGrid: some View {
+        LazyVGrid(
+            columns: Array(repeating: GridItem(.flexible(), spacing: 16), count: 3),
+            alignment: .center,
+            spacing: 16,
+            pinnedViews: [.sectionHeaders],
+            content: {
+                Section(content: {
+                    ForEach(profileColors, id: \.self) { color in
+                        Circle()
+                            .fill(.accent)
+                            .overlay(
+                                color
+                                    .clipShape(Circle())
+                                    .padding(selectedColor == color ? 10 : 0)
+                            )
+                            .onTapGesture {
+                                selectedColor = color
+                            }
+                    }
+                }, header: {
+                    Text("Select a profile color")
+                        .font(.headline)
+                })
+            }
+        )
     }
     
     private var ctaButton: some View {

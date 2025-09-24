8dbe09d59d10c21b4dfe74c9d59030efb3fa511e
vanya <vanya.moo@gmail.com>
2025-09-24 14:46:41 +0100

    [Feature] Add ColorView folder, add `OnboardingColorView` file

---
+++ AIChat/Core/Onboarding/ColorView/OnboardingColorView.swift
@@ -0,0 +1,40 @@
+//
+//  OnboardingColorView.swift
+//  AIChat
+//
+//  Created by Vanya Mutafchieva on 24/09/2025.
+//
+
+import SwiftUI
+
+struct OnboardingColorView: View {
+    
+    let profileColors: [Color] = [.red, .green, .orange, .blue, .mint, .purple, .cyan, .teal, .indigo]
+    
+    var body: some View {
+        ScrollView {
+            LazyVGrid(
+                columns: Array(repeating: GridItem(.flexible(), spacing: 16), count: 3),
+                alignment: .center,
+                spacing: 16,
+                pinnedViews: [.sectionHeaders],
+                content: {
+                    Section(content: {
+                        ForEach(profileColors, id: \.self) { color in
+                            Circle()
+                                .fill(color)
+                        }
+                    }, header: {
+                        Text("Select a profile color")
+                            .font(.headline)
+                    })
+                }
+            )
+            .padding(.horizontal, 24)
+        }
+    }
+}
+
+#Preview {
+    OnboardingColorView()
+}

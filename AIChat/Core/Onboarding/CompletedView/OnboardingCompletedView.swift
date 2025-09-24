8633f676d7a405dc29cfe28dca5082950d9ff66f
vanya <vanya.moo@gmail.com>
2025-09-24 16:34:09 +0100

    [Feature] Add setup complete text to OnboardingCompletedView

--- AIChat/Core/Onboarding/CompletedView/OnboardingCompletedView.swift
+++ AIChat/Core/Onboarding/CompletedView/OnboardingCompletedView.swift
@@ -11,23 +11,34 @@ struct OnboardingCompletedView: View {
     
     @Environment(AppState.self) private var root
     
+    var selectedColor: Color = .orange
+    
     var body: some View {
-        VStack {
-            Text("Onboarding Completed!")
-                .frame(maxHeight: .infinity)
+        VStack(alignment: .leading, spacing: 12) {
+            Text("Setup complete!")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundStyle(selectedColor)
             
-            
-
+            Text("We've set up your profile and you're ready to start chatting")
+                .font(.title)
+                .fontWeight(.medium)
+                .foregroundStyle(.secondary)
         }
+        .frame(maxHeight: .infinity)
         .safeAreaInset(edge: .bottom, content: {
-            Button {
-                onFinishButtonPressed()
-            } label: {
-                Text("Finish")
-                    .callToActionButton()
-            }
+            ctaButton
         })
-        .padding(16)
+        .padding(24)
+    }
+    
+    private var ctaButton: some View {
+        Button {
+            onFinishButtonPressed()
+        } label: {
+            Text("Finish")
+                .callToActionButton()
+        }
     }
     
     func onFinishButtonPressed() {

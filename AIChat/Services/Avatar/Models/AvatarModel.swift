fd58a1c89eb1224f521e011a3eb9db6666c3f888
vanya <vanya.moo@gmail.com>
2025-09-25 16:53:37 +0100

    [Feature] Add properties and custom initializer to AvatarModel

--- AIChat/Services/Avatar/Models/AvatarModel.swift
+++ AIChat/Services/Avatar/Models/AvatarModel.swift
@@ -5,3 +5,48 @@
 //  Created by Vanya Mutafchieva on 25/09/2025.
 //
 
+import Foundation
+
+struct AvatarModel {
+    
+    let avatarId: String
+    let name: String?
+    let characterOption: CharacterOption?
+    let characterAction: CharacterAction?
+    let characterLoaction: CharacterLocation?
+    let profileImageName: String?
+    let authorId: String?
+    let dateCreated: Date?
+    
+    init(
+        avatarId: String,
+        name: String? = nil,
+        characterOption: CharacterOption? = nil,
+        characterAction: CharacterAction? = nil,
+        characterLoaction: CharacterLocation? = nil,
+        profileImageName: String? = nil,
+        authorId: String? = nil,
+        dateCreated: Date? = nil
+    ) {
+        self.avatarId = avatarId
+        self.name = name
+        self.characterOption = characterOption
+        self.characterAction = characterAction
+        self.characterLoaction = characterLoaction
+        self.profileImageName = profileImageName
+        self.authorId = authorId
+        self.dateCreated = dateCreated
+    }
+}
+
+enum CharacterOption {
+    case man, woman, alien, cat, dog
+}
+
+enum CharacterAction {
+    case smiling, sitting, eating, drinking, walking, shopping, studying, working, relaxing, fighting, crying
+}
+
+enum CharacterLocation {
+    case office, home, park, beach, city, forest, mountain
+}

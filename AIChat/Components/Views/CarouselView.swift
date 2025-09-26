82b33ede7cbce9c9386e979529c44a31e61e9bbc
vanya <vanya.moo@gmail.com>
2025-09-26 15:05:27 +0100

    [Feature] Make AvatarModel Hashable

---
+++ AIChat/Components/Views/CarouselView.swift
@@ -0,0 +1,18 @@
+//
+//  CarouselView.swift
+//  AIChat
+//
+//  Created by Vanya Mutafchieva on 26/09/2025.
+//
+
+import SwiftUI
+
+struct CarouselView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CarouselView()
+}
--- AIChat/Services/Avatar/Models/AvatarModel.swift
+++ AIChat/Services/Avatar/Models/AvatarModel.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct AvatarModel {
+struct AvatarModel: Hashable {
     
     let avatarId: String
     let name: String?

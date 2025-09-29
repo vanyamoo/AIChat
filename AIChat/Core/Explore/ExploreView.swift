0dc588e152135a6fa6099f85155d7e02351ed9cd
vanya <vanya.moo@gmail.com>
2025-09-29 17:02:31 +0100

    [Feature] Add `categorySection` to List in ExploreView

--- AIChat/Core/Explore/ExploreView.swift
+++ AIChat/Core/Explore/ExploreView.swift
@@ -10,12 +10,15 @@ import SwiftUI
 struct ExploreView: View {
     
     @State private var featuredAvatars: [AvatarModel] = AvatarModel.mocks
+    @State private var categories: [CharacterOption] = CharacterOption.allCases
     
     var body: some View {
         NavigationStack {
             List {
                 
                 featuredSection
+                
+                categorySection
                 
             }
             .navigationTitle("Explore")
@@ -38,6 +41,31 @@ struct ExploreView: View {
             
         } header: {
             Text("Featured Avatars")
+        }
+    }
+    
+    private var categorySection: some View {
+        Section {
+            ZStack {
+                ScrollView(.horizontal) {
+                    HStack(spacing: 12) {
+                        ForEach(categories, id: \.self) { category in
+                            CategoryCellView(
+                                title: category.rawValue.capitalized,
+                                imageName: Constants.randomImage
+                            )
+                        }
+                    }
+                }
+                .frame(height: 140)
+                .scrollIndicators(.hidden)
+                .scrollTargetLayout()
+                .scrollTargetBehavior(.viewAligned)
+            }
+            .removeListRowFormatting()
+            
+        } header: {
+            Text("Categories")
         }
     }
 }

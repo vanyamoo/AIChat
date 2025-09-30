f46b1d6c8e2ea8ff505f4a93e2948deb30a39f94
vanya <vanya.moo@gmail.com>
2025-09-30 16:00:24 +0100

    Make `imageName` Optional in `CustomListCellView`, fix padding

--- AIChat/Components/Views/CustomListCellView.swift
+++ AIChat/Components/Views/CustomListCellView.swift
@@ -9,16 +9,24 @@ import SwiftUI
 
 struct CustomListCellView: View {
     
-    var imageName: String = Constants.randomImage
+    var imageName: String? = Constants.randomImage
     var title: String? = "Alpha"
     var subtitle: String? = "An alien that is smiling in the park"
     
     var body: some View {
         HStack {
-            ImageLoaderView(urlString: imageName)
-                .aspectRatio(contentMode: .fit)
-                .frame(height: 60)
-                .cornerRadius(16)
+            ZStack {
+                if let imageName {
+                    ImageLoaderView(urlString: imageName)
+                } else {
+                    Rectangle()
+                        .fill(.secondary.opacity(0.5))
+                }
+            }
+            .aspectRatio(contentMode: .fit)
+            .frame(height: 60)
+            .cornerRadius(16)
+            
             VStack(alignment: .leading, spacing: 4) {
                 if let title {
                     Text(title)
@@ -29,10 +37,10 @@ struct CustomListCellView: View {
                          .font(.subheadline)
                 }
             }
-            .padding(12)
-            .padding(.vertical, 4)
             .frame(maxWidth: .infinity, alignment: .leading)
         }
+        .padding(12)
+        .padding(.vertical, 4)
         .background(Color(.systemBackground))
     }
 }
@@ -40,6 +48,12 @@ struct CustomListCellView: View {
 #Preview {
     ZStack {
         Color.gray.ignoresSafeArea()
-        CustomListCellView()
+        
+        VStack {
+            CustomListCellView()
+            CustomListCellView(imageName: nil)
+            CustomListCellView(title: nil)
+            CustomListCellView(subtitle: nil)
+        }
     }
 }

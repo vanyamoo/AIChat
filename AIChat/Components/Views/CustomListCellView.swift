784d218884c99ad75482f15f5b7c4ad6f54b4ef9
vanya <vanya.moo@gmail.com>
2025-09-30 15:42:45 +0100

    Add `CustomListCellView` in Components/View

--- AIChat/Components/Views/CustomListCellView.swift
+++ AIChat/Components/Views/CustomListCellView.swift
@@ -8,11 +8,38 @@
 import SwiftUI
 
 struct CustomListCellView: View {
+    
+    var imageName: String = Constants.randomImage
+    var title: String? = "Alpha"
+    var subtitle: String? = "An alien that is smiling in the park"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            ImageLoaderView(urlString: imageName)
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 60)
+                .cornerRadius(16)
+            VStack(alignment: .leading, spacing: 4) {
+                if let title {
+                    Text(title)
+                        .font(.headline)
+                }
+                 if let subtitle {
+                     Text(subtitle)
+                         .font(.subheadline)
+                }
+            }
+            .padding(12)
+            .padding(.vertical, 4)
+            .frame(maxWidth: .infinity, alignment: .leading)
+        }
+        .background(Color(.systemBackground))
     }
 }
 
 #Preview {
-    CustomListCellView()
+    ZStack {
+        Color.gray.ignoresSafeArea()
+        CustomListCellView()
+    }
 }

14930d416d4c806cc65ccfc3cde81fa2b65cb152
vanya <vanya.moo@gmail.com>
2025-09-30 16:28:03 +0100

    Add new file `ButtonViewModifiers` in Components/ViewModifiers, move file `View+EXT` to new ViewModifiers folder

---
+++ AIChat/Components/ViewModifiers/ButtonViewModifiers.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonViewModifiers.swift
+//  AIChat
+//
+//  Created by Vanya Mutafchieva on 30/09/2025.
+//
+
+import SwiftUI
+
+struct ButtonViewModifiers: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ButtonViewModifiers()
+}

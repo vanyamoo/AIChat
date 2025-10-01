3c6e61ebdeec5a18ac1e1f7c76e37a5c06c2893a
vanya <vanya.moo@gmail.com>
2025-10-01 12:47:28 +0100

    Add `Date+EXT` file in Extensions folder, move `func addingTimeInterval(days:, hours:, minutes:)` to `Date+EXT` file

--- AIChat/Services/Chat/Models/ChatModel.swift
+++ AIChat/Services/Chat/Models/ChatModel.swift
@@ -7,15 +7,6 @@
 
 import Foundation
 
-extension Date {
-    func addingTimeInterval(days: Int = 0, hours: Int = 0, minutes: Int = 0) -> Date {
-        let dayInterval = TimeInterval(days * 24 * 60 * 60)
-        let hourInterval = TimeInterval(hours * 60 * 60)
-        let minuteInterval = TimeInterval(minutes * 60)
-        return self.addingTimeInterval(dayInterval + hourInterval + minuteInterval)
-    }
-}
-
 struct ChatModel {
     let id: String
     let userId: String // the user that created the chat

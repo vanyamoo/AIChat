90d64d54e919642ea46c056ec64dbf42d67faf9c
vanya <vanya.moo@gmail.com>
2025-10-04 12:07:40 +0100

    Add `myAvatarsSection` in `ProfileView`

--- AIChat/Core/Profile/ProfileView.swift
+++ AIChat/Core/Profile/ProfileView.swift
@@ -10,12 +10,16 @@ import SwiftUI
 struct ProfileView: View {
     
     @State private var showSettingsView: Bool = false
+    @State private var showCreateAvatarView: Bool = false
     @State private var currentUser: UserModel? = .mock
+    @State private var myAvatars: [AvatarModel] = AvatarModel.mocks
     
     var body: some View {
         NavigationStack {
             List {
                 myInfoSection
+                
+                myAvatarsSection
             }
                 .navigationTitle("Profile")
                 .toolbar {
@@ -27,6 +31,9 @@ struct ProfileView: View {
         }
         .sheet(isPresented: $showSettingsView) {
             SettingsView()
+        }
+        .fullScreenCover(isPresented: $showCreateAvatarView) {
+            Text("Create Avatar")
         }
     }
     
@@ -43,6 +50,37 @@ struct ProfileView: View {
         }
     }
     
+    private var myAvatarsSection: some View {
+        Section {
+            ForEach(myAvatars, id: \.self) { avatar in
+                CustomListCellView(
+                    imageName: avatar.profileImageName,
+                    title: avatar.name,
+                    subtitle: nil
+                )
+                .anyButton(.highlight, action: {
+                    
+                })
+                .removeListRowFormatting()
+            }
+            .onDelete { indexSet in
+                onDeleteAvatar(indexSet: indexSet)
+            }
+            
+        } header: {
+            HStack(spacing: 0) {
+                Text("My Avatars")
+                Spacer()
+                Image(systemName: "plus.circle.fill")
+                    .font(.title)
+                    .foregroundStyle(.accent)
+                    .anyButton {
+                        onNewAvatarButtonPressed()
+                    }
+            }
+        }
+    }
+    
     private var settingsButton: some View {
         Button {
             onSettingsButtonPressed()
@@ -55,8 +93,18 @@ struct ProfileView: View {
     private func onSettingsButtonPressed() {
         showSettingsView = true
     }
+    
+    private func onNewAvatarButtonPressed() {
+        showCreateAvatarView = true
+    }
+    
+    private func onDeleteAvatar(indexSet: IndexSet) {
+        guard let index = indexSet.first else { return }  // myAvatars.remove(atOffsets: indexSet)
+        myAvatars.remove(at: index)
+    }
 }
 
 #Preview {
     ProfileView()
+        .environment(AppState())
 }

546c4d7463c8e6d5b2f1c794d7ca11467d8705d0
vanya <vanya.moo@gmail.com>
2025-10-06 12:55:50 +0100

    Make `showCreateAccountView` a half-sheet

--- AIChat/Core/Settings/SettingsView.swift
+++ AIChat/Core/Settings/SettingsView.swift
@@ -40,6 +40,7 @@ struct SettingsView: View {
         }
         .sheet(isPresented: $showCreateAccountView) {
             CreateAccountView()
+                .presentationDetents([.medium])
         }
     }
     

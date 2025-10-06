e66fbf2bb0791745863af52b6ebe6101abbde743
vanya <vanya.moo@gmail.com>
2025-10-06 13:06:07 +0100

    Tidy up

--- AIChat/Core/CreateAccount/CreateAccountView.swift
+++ AIChat/Core/CreateAccount/CreateAccountView.swift
@@ -9,33 +9,30 @@ import SwiftUI
 
 struct CreateAccountView: View {
     var title: String = "Create Account?"
-        var subtitle: String = "Don't lose your data! Connect to an SSO provider to save your account."
-        
-        var body: some View {
-            VStack(spacing: 24) {
-                VStack(alignment: .leading, spacing: 8) {
-                    Text(title)
-                        .font(.largeTitle)
-                        .fontWeight(.semibold)
-                    Text(subtitle)
-                        .font(.body)
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                
-                SignInWithAppleButtonView(
-                    type: .signIn,
-                    style: .black,
-                    cornerRadius: 10
-                )
-                .frame(height: 55)
-                .anyButton(.press) {
-                    
-                }
-                
-                Spacer()
+    var subtitle: String = "Don't lose your data! Connect to an SSO provider to save your account."
+    
+    var body: some View {
+        VStack(spacing: 24) {
+            VStack(alignment: .leading, spacing: 8) {
+                Text(title)
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                Text(subtitle)
+                    .font(.body)
             }
-            .padding(16)
-            .padding(.top, 40)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            SignInWithAppleButtonView(
+                type: .signIn,
+                style: .black,
+                cornerRadius: 10
+            )
+            .frame(height: 55)
+            .anyButton(.press) {
+                
+            }
+            
+            Spacer()
         }
 }
 

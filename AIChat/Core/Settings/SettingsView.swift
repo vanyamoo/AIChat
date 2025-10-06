b7ff006af1c2508341387276b49ae36305bff678
vanya <vanya.moo@gmail.com>
2025-10-06 12:21:26 +0100

    Add three sections to `SettingsView` - `accountSection, purchasesSection, applicationSection`

--- AIChat/Core/Settings/SettingsView.swift
+++ AIChat/Core/Settings/SettingsView.swift
@@ -12,20 +12,99 @@ struct SettingsView: View {
     @Environment(\.dismiss) private var dismiss
     @Environment(AppState.self) private var appState: AppState
     
+    @State private var isPremium: Bool = false
+    
     var body: some View {
         NavigationStack {
             List {
-                Button {
-                    onSignOutTapped()
-                } label: {
-                    Text("Sign Out")
-                }
+                accountSection
+                
+                purchasesSection
+                
+                applicationSection
             }
             .navigationTitle("Settings")
         }
     }
     
-    func onSignOutTapped() {
+    private var accountSection: some View {
+        Section {
+            Text("Sign Out")
+                .rowFormatting()
+                .anyButton(.highlight) {
+                    onSignOutPressed()
+                }
+                .removeListRowFormatting()
+            
+            Text("Delete Account")
+                .foregroundStyle(.red)
+                .rowFormatting()
+                .anyButton(.highlight) {
+                    onDeleteAccountPressed()
+                }
+                .removeListRowFormatting()
+        } header: {
+            Text("Account")
+        }
+    }
+    
+    private var purchasesSection: some View {
+        Section {
+            HStack {
+                Text("Account status: \(isPremium ? "PREMIUM" : "FREE")")
+                Spacer(minLength: 0)
+                if isPremium {
+                    Text("MANAGE")
+                        .badgeButton()
+                }
+            }
+            .rowFormatting()
+            .anyButton(.highlight) {
+                
+            }
+            .disabled(!isPremium)
+            .removeListRowFormatting()
+        } header: {
+            Text("Purchases")
+        }
+    }
+    
+    private var applicationSection: some View {
+        Section {
+            HStack {
+                Text("Version")
+                Spacer(minLength: 0)
+                Text(Utilities.appVersion ?? "")
+                    .foregroundStyle(.secondary)
+            }
+            .rowFormatting()
+            .removeListRowFormatting()
+            
+            HStack {
+                Text("Build Number")
+                Spacer(minLength: 0)
+                Text(Utilities.buildNumber ?? "")
+                    .foregroundStyle(.secondary)
+            }
+            .rowFormatting()
+            .removeListRowFormatting()
+            
+            Text("Contact us")
+                .foregroundStyle(.blue)
+                .rowFormatting()
+                .anyButton(.highlight, action: {
+                    
+                })
+                .removeListRowFormatting()
+        } header: {
+            Text("Application")
+        } footer: {
+            Text("Created by Tiny Empire. \nLearn more at https://tinyempire.ie")
+                .baselineOffset(6)
+        }
+    }
+    
+    func onSignOutPressed() {
         // do some logic to sign user out of app!
         dismiss()
         
@@ -33,6 +112,10 @@ struct SettingsView: View {
             try? await Task.sleep(for: .seconds(1))
             appState.updateViewState(showTabBarView: false)
         }
+    }
+    
+    func onDeleteAccountPressed() {
+        
     }
 }
 

699627889fb2f7e4be95adfb40ffa949eb964e50
vanya <vanya.moo@gmail.com>
2025-10-08 10:17:30 +0100

    Add `ChatView` file in new folder Core/Chat

--- AIChat/Core/Chat/ChatView.swift
+++ AIChat/Core/Chat/ChatView.swift
@@ -8,11 +8,39 @@
 import SwiftUI
 
 struct ChatView: View {
+    
+    @State private var chatMessages: [ChatMessageModel] = ChatMessageModel.mocks
+    @State private var avatar: AvatarModel? = .mock
+    @State private var currentUser: UserModel? = .mock
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 0) {
+            ScrollView {
+                LazyVStack(spacing: 24) {
+                    ForEach(chatMessages) { message in
+                        let isCurrentUser = message.authorId == currentUser?.userId
+                        ChatBubbleViewBuilder(
+                            message: message,
+                            isCurrentUser: isCurrentUser,
+                            imageName: isCurrentUser ? nil : avatar?.profileImageName
+                        )
+                    }
+                }
+            }
+            .frame(maxWidth: .infinity)
+            .padding(8)
+            
+            Rectangle()
+                .frame(height: 50)
+                
+        }
+        .navigationTitle(avatar?.name ?? "Chat")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 #Preview {
-    ChatView()
+    NavigationStack {
+        ChatView()
+    }
 }

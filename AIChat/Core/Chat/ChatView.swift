20290362e9e5506a88c7f9cff07b2e83fdd798f4
vanya <vanya.moo@gmail.com>
2025-10-09 12:24:05 +0100

    Extract `Binding((ifNotNil:)` extension code into a new file `Binding+EXT` in Extensions folder

--- AIChat/Core/Chat/ChatView.swift
+++ AIChat/Core/Chat/ChatView.swift
@@ -7,21 +7,6 @@
 
 import SwiftUI
 
-extension Binding where Value == Bool {
-    init<T: Sendable>(ifNotNil value: Binding<T?>) {
-        self.init(
-            get: {
-                value.wrappedValue != nil
-            },
-            set: { newValue in
-                if !newValue {
-                    value.wrappedValue = nil
-                }
-            }
-        )
-    }
-}
-
 struct ChatView: View {
     
     @State private var chatMessages: [ChatMessageModel] = ChatMessageModel.mocks

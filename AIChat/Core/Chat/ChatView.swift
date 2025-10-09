174a125ea15330adc212f93aa890fbbc0478456c
vanya <vanya.moo@gmail.com>
2025-10-09 12:20:35 +0100

    Extract `AnyAppAlert`, `AlertType` and `showCustomAlert` code into a new file in new folder Components/Alerts

--- AIChat/Core/Chat/ChatView.swift
+++ AIChat/Core/Chat/ChatView.swift
@@ -22,64 +22,6 @@ extension Binding where Value == Bool {
     }
 }
 
-struct AnyAppAlert: Sendable {
-    var title: String
-    var subtitle: String?
-    var buttons: (@Sendable () -> AnyView)?
-    
-    init(
-        title: String,
-        subtitle: String? = nil,
-        buttons: (@Sendable () -> AnyView)? = nil
-    ) {
-        self.title = title
-        self.subtitle = subtitle
-        self.buttons = buttons ?? {
-            AnyView(
-                Button("OK") {
-                    
-                }
-            )
-        }
-    }
-    
-    init(error: Error) {
-        self.init(title: "Error", subtitle: error.localizedDescription, buttons: nil)
-    }
-}
-
-enum AlertType {
-    case alert
-    case confirmationDialog
-}
-
-extension View {
-    @ViewBuilder
-    func showCustomAlert(type: AlertType = .alert, alert: Binding<AnyAppAlert?>) -> some View {
-        switch type {
-        case .alert:
-            self
-                .alert(alert.wrappedValue?.title ?? "", isPresented: Binding(ifNotNil: alert)) {
-                    alert.wrappedValue?.buttons?()
-                }
-            message: {
-                if let subtitle = alert.wrappedValue?.subtitle {
-                    Text(subtitle)
-                }
-            }
-        case .confirmationDialog:
-            self
-                .confirmationDialog(alert.wrappedValue?.title ?? "", isPresented: Binding(ifNotNil: alert)) {
-                    alert.wrappedValue?.buttons?()
-                } message: {
-                    if let subtitle = alert.wrappedValue?.subtitle {
-                        Text(subtitle)
-                    }
-                }
-        }
-    }
-}
-
 struct ChatView: View {
     
     @State private var chatMessages: [ChatMessageModel] = ChatMessageModel.mocks

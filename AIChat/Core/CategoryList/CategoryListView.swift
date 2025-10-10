e747df914056e9ae2a4320205111d46a8b8a7112
vanya <vanya.moo@gmail.com>
2025-10-10 11:35:11 +0100

    Add `CategoryListView` file in new folder Core/CategoryList

--- AIChat/Core/CategoryList/CategoryListView.swift
+++ AIChat/Core/CategoryList/CategoryListView.swift
@@ -8,11 +8,37 @@
 import SwiftUI
 
 struct CategoryListView: View {
+    
+    var category: CharacterOption = .alien
+    var imageName: String = Constants.randomImage
+    @State private var avatars: [AvatarModel] = AvatarModel.mocks
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            CategoryCellView(
+                title: category.plural.capitalized,
+                imageName: imageName,
+                font: .largeTitle,
+                cornerRadius: 0
+            )
+            .removeListRowFormatting()
+            
+            ForEach(avatars, id: \.self) { avatar in
+                CustomListCellView(
+                    imageName: avatar.profileImageName,
+                    title: avatar.name,
+                    subtitle: avatar.characterDescription
+                )
+                .removeListRowFormatting()
+            }
+        }
+        .listStyle(.plain)
+        .ignoresSafeArea()
     }
 }
 
 #Preview {
-    CategoryListView()
+    NavigationStack {
+        CategoryListView()
+    }
 }

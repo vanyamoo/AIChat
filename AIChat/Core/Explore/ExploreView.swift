139f3dd901252789c8c9aba74d55bcdd4fe99b8f
vanya <vanya.moo@gmail.com>
2025-10-10 13:51:18 +0100

    Extract custom modifier `.navigationDestinationForCoreModule` in new file `NavigationPathOption` in new folder Core/Routing

--- AIChat/Core/Explore/ExploreView.swift
+++ AIChat/Core/Explore/ExploreView.swift
@@ -7,25 +7,6 @@
 
 import SwiftUI
 
-enum NavigationPathOption: Hashable {
-    case chat(avatarId: String)
-    case category(category: CharacterOption, imageName: String)
-}
-
-extension View {
-    func navigationDestinationForCoreModule(path: Binding<[NavigationPathOption]>) -> some View {
-        self
-            .navigationDestination(for: NavigationPathOption.self) { newValue in
-                switch newValue {
-                case .chat(avatarId: let avatarId):
-                    ChatView(avatarId: avatarId)
-                case .category(category: let category, imageName: let imageName):
-                    CategoryListView(path: path, category: category, imageName: imageName)
-                }
-            }
-    }
-}
-
 struct ExploreView: View {
     
     @State private var featuredAvatars: [AvatarModel] = AvatarModel.mocks
---
+++ AIChat/Core/Routing/NavigationPathOption.swift
@@ -0,0 +1,27 @@
+//
+//  NavigationPathOption.swift
+//  AIChat
+//
+//  Created by Vanya Mutafchieva on 10/10/2025.
+//
+
+import SwiftUI
+
+enum NavigationPathOption: Hashable {
+    case chat(avatarId: String)
+    case category(category: CharacterOption, imageName: String)
+}
+
+extension View {
+    func navigationDestinationForCoreModule(path: Binding<[NavigationPathOption]>) -> some View {
+        self
+            .navigationDestination(for: NavigationPathOption.self) { newValue in
+                switch newValue {
+                case .chat(avatarId: let avatarId):
+                    ChatView(avatarId: avatarId)
+                case .category(category: let category, imageName: let imageName):
+                    CategoryListView(path: path, category: category, imageName: imageName)
+                }
+            }
+    }
+}
